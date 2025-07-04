349e0c4c24eb8f79f75f7a426ab40437f7bbe867
benjamin <133883483+mmmafla@users.noreply.github.com>
2025-07-03 23:44:14 -0400

    pt5

--- src/app/veterinario/dashboard/dashboard.page.ts
+++ src/app/veterinario/dashboard/dashboard.page.ts
@@ -3,6 +3,7 @@ import { RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from 'src/app/componentes/header/header.component';
+import { ChartComponent } from 'src/app/componentes/chart/chart.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 // Supabase
@@ -13,6 +14,17 @@ const supabase = createClient(
   'https://irorlonysbmkbdthvrmt.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlyb3Jsb255c2Jta2JkdGh2cm10Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYyODgwMDQsImV4cCI6MjA2MTg2NDAwNH0.s-ZEteHxMWX43NCQIuNmTWpbBoEUxseKyg_YaXpi6Ek'
 );
+
+interface Estadistica {
+  total_atenciones: number;
+  motivos: { [key: string]: number };
+  sistemas: { [key: string]: number };
+  alimentacion: { [key: string]: number };
+  estado_sensorial: { [key: string]: number };
+  especies?: { [key: string]: number }; // opcional
+  temperatura_prom: number;
+  peso_prom: number;
+}
 
 @Component({
   selector: 'app-dashboard',
@@ -25,11 +37,12 @@ const supabase = createClient(
     FormsModule,
     ReactiveFormsModule,
     HeaderComponent,
+    ChartComponent,
   ]
 })
 export class DashboardPage implements OnInit {
 
-  estadisticas: any;
+  estadisticas: Estadistica | null = null;
   cargando: boolean = false;
 
   mesSeleccionado: number = new Date().getMonth() + 1; // mes actual (1-12)
@@ -39,12 +52,13 @@ export class DashboardPage implements OnInit {
     'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'
   ];
   anios: number[] = [];
+  private cantidadAnios = 50;
 
   constructor() {}
 
   async ngOnInit() {
     const añoActual = new Date().getFullYear();
-    this.anios = Array.from({ length: 5 }, (_, i) => añoActual - i);
+    this.anios = Array.from({ length: this.cantidadAnios }, (_, i) => añoActual - i);
     await this.obtenerEstadisticasDashboard();
   }
 
@@ -92,6 +106,8 @@ export class DashboardPage implements OnInit {
       console.error('Error al obtener estadísticas:', error);
     } else {
       this.estadisticas = data;
+      console.log('Datos para el gráfico:', this.getEspeciesData());
+      console.log('Etiquetas para el gráfico:', this.getEspeciesLabels());
       console.log('Estadísticas cargadas:', this.estadisticas);
     }
 
@@ -100,5 +116,13 @@ export class DashboardPage implements OnInit {
 
   getKeys(obj: any): string[] {
     return obj ? Object.keys(obj) : [];
+  }
+
+  getEspeciesData(): number[] {
+    return this.estadisticas?.especies ? Object.values(this.estadisticas.especies) : [];
+  }
+
+  getEspeciesLabels(): string[] {
+    return this.estadisticas?.especies ? Object.keys(this.estadisticas.especies) : [];
   }
 }
